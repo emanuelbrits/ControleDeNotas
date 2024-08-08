c084b9c0e5935b9f96a43198cdb8488e489088e5
italoprobo <italoprobo@gmail.com>
2024-08-08 15:58:41 -0300

    start front-end

---
+++ FrontEnd/ControleDeNotas/src/pages/homePage/homepage.tsx
@@ -0,0 +1,12 @@
+function HomePage() {
+
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold text-blue-500">Tailwind CSS está funcionando!</h1>
+      </div>
+    </>
+  )
+}
+
+export default HomePage
